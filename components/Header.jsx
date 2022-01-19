b3c53aedf4fdbdb36e04ca36adc4eddf03564fce
Ricardo Poullet <rickypoullet@gmail.com>
2022-01-19 18:36:46 -0500

    added search calender and server sided fetching for listings. Added page loading bar.

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,11 +6,53 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/router";
 
-function Header() {
+function Header({ placeholder }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numOfGuests, setNumOfGuests] = useState(1);
+  const router = useRouter();
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numOfGuests,
+      },
+    });
+    resetInput();
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src={"https://links.papareact.com/qd3"}
           layout="fill"
@@ -23,10 +65,13 @@ function Header() {
       {/* middle */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your search"
+          placeholder={!placeholder ? "Start your search" : placeholder}
         />
+
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
@@ -35,12 +80,49 @@ function Header() {
         <p className="hidden md:inline cursor-pointer text-center">
           Become a host
         </p>
+
         <GlobeAltIcon className="h-6 cursor-pointer" />
+
         <div className="flex border-2 rounded-full p-2 items-center cursor-pointer">
           <MenuIcon className="h-6" />
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex border-b-2 mb-4 items-center">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+
+            <UsersIcon className="h-5" />
+            <input
+              className="w-9 text-center outline-none text-red-400"
+              type="number"
+              value={numOfGuests}
+              onChange={(e) => setNumOfGuests(e.target.value)}
+              min={1}
+              name=""
+              id=""
+            />
+          </div>
+          <div className="flex justify-evenly">
+            <button onClick={resetInput} className="text-gray-500">
+              Cancel
+            </button>
+            <button onClick={search} className="text-red-500">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
